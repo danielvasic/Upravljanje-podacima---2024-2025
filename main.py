4dc7d1932f5920c865a92e93ddc75b630964368c
danielvasic <dvasic1@gmail.com>
2024-11-11 15:13:32 +0100

    feat: add user registration endpoint and update users retrieval

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ def create_user(user: User):
     users_collection.insert_one(user.dict())
     return user
 
+@app.post("/register", response_model=LoginUser)
+def register_user(user: LoginUser):
+    users_collection.insert_one(user.dict())
+    return user
+
 @app.get("/users", response_model=List[User])
 def get_users():
-    return users_collection.find()
+    return list(users_collection.find())
